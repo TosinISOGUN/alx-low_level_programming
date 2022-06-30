41d615715da5da25f039ea278c9680c4f5337179
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-06-30 12:09:17 +0000

    Write a C program that prints exactly and that piece of art is useful - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 return (1);
 }
