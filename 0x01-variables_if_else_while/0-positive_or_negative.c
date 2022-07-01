fdb3faa26944a0ba3210154e12f5d58e9cb909e2
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-07-01 11:33:38 +0000

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print whether the number stored in the variable n is positive or negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
 * main -> This program will assign a random number to the variable n each time it is executed and prints out
 * based a condition
-* Return : always 0 
+* Return : always 0
 */
 int main(void)
 {
