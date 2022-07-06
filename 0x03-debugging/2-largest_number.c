06f8d6567560a31d3b6adc295bf4907ecc9492c4
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-07-06 18:28:44 +0100

    Debugging task 2, Largest of three integers

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,27 +1,30 @@
+#include "main.h"
+
 /**
  * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
+ * @var1: first integer
+ * @var2: second integer
+ * @var3: third integer
+ *
+ * Created by: ALX
+ * Modified by: TosinISOGUN
+ * Date modified: 6th July, 2022
+ *
  * Return: largest number
  */
 
-int largest_number(int a, int b, int c)
+int largest_number(int var1, int var2, int var3)
 {
 	int largest;
 
-	if (a > b && a > c)
-	{
-		largest = a;
-	}
-	else if (b > a && b > c)
-	{
-		largest = b;
-	}
+	if (var1 > var2 && var1 > var3)
+		largest = var1;
+	else if (var2 > var1 && var2 > var3)
+		largest = var2;
+	else if (var3 > var2)
+		largest = var3;
 	else
-	{
-		largest = c;
-	}
+		largest = var2;
 
 	return (largest);
 }
