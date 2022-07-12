3c246079a873cd58ffb018f4d89b2a8f1869d088
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-07-12 09:57:16 +0100

    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,21 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
+ * main -> a program that prints the alphabets in lowercase,
  * followed by a new line
+ * Return: Always 0
  */
-void print_numbers(void)
-{
-	int i;
+ int main(void)
+ {
 
-	for (i = 0; i < 10; i++)
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		_putchar(i + '0');
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+		putchar(10);
+		return (0);
 	}
-
-	_putchar('\n');
-}
+ }
