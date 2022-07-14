cb670c25b1ecd27707454ec69f80608c51c778ed
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-07-14 20:15:29 +0100

    a program that prints a[2] = 98, followed by a new line.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-/**
- * main -> Entry point
- *
- * Description: the program prints a[2] = 98, followed by a new line.
- *
- * Return: Always 0 (Success)
- */
-
 int main(void)
 {
 	int n;
