6974cf6c0db323834af13f8355d16bdf99a3dc22
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-07-24 09:18:24 +0100

    a function that returns a pointer to a 2 dimensional array of integers.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - make a 2-D array dynamically
+ * @col: column of the array
+ * @row: row of the array
+ *
+ * Return: pointer to the 2-D array
+ *
+ * Created by: Sanctus-Peter
+ * cc: 23rd July, 2022
+ */
+
+int **alloc_grid(int col, int row)
+{
+	int i, j,  **grid;
+
+	grid = malloc(row * sizeof(*grid));
+
+	if (row < 1 || col < 1 || grid == 0)
+		return (NULL);
+
+	for (i = 0; i < row; i++)
+	{
+		grid[i] = malloc(col * sizeof(**grid));
+		if (grid[i] == 0)
+		{
+			while (i--)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < col; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
