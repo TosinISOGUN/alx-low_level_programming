528b1a8f5ecc3faeb4c9e8fbe33d27ae35288641
TosinISOGUN <oluwatomisinisogun@gmail.com>
2022-08-03 23:19:47 +0100

    a header file containing the function prototypes.

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
